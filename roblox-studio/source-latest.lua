f3291a38381dd4e1e690da7549d74da30d80b2b6
awesomedude939 <96753200+awesomedude939@users.noreply.github.com>
2022-02-16 18:43:01 -0300

    Update source-latest.lua

--- roblox-studio/source-latest.lua
+++ roblox-studio/source-latest.lua
@@ -36,7 +36,6 @@ for i,v1 in pairs(AllowedInstances) do
 end
 WriteScript("local InstanceTable = {}")
 for i1,v1 in pairs(CleanInstances) do 
-	WriteScript("spawn(function()")
 	local sendTo = ""
 	local needed = true
 	sendTo = sendTo.."local InstanceV"
@@ -83,7 +82,5 @@ for i1,v1 in pairs(CleanInstances) do
 			end)
 		end
 	end
-
-	WriteScript("end)")
 end
 print(Script)
